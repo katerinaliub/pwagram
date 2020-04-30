e96c648397f4ad31f4bad5e6ef296cc5c6925786
Katerina Liubarshchuk <katerina.holodna@gmail.com>
2020-04-30 18:14:37 +0200

    cleanup

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,10 @@
 self.addEventListener('install', function (event) {
-    console.log('[SW] Installing SW ...', event);
 });
 
 self.addEventListener('activate', function (event) {
-    console.log('[SW] Activating SW ...', event);
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', function (event) {
-    console.log('[SW] Fetching something ...', event);
     event.respondWith(fetch(event.request));
 });

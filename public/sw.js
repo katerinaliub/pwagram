5d47cd40fac58cad2f0d370c1844630796cd1174
Katerina Liubarshchuk <katerina.holodna@gmail.com>
2020-05-06 14:53:42 +0200

    responsive design

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v20';
+var CACHE_STATIC_NAME = 'static-v25';
 var CACHE_DYNAMIC_NAME = 'dynamic-v5';
 var STATIC_FILES = [
     '/',
